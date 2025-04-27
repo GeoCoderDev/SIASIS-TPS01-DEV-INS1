37b00446dfc46715e1898c1def06d482c75ef9d8
Juanito3500 <juanchavezsaldana1@gmail.com>
2025-04-27 13:03:26 -0500

    Probando implementacion multi BD en TPS01 6

--- src/constants/RDP02_INSTANCES_DISTRIBUTION.ts
+++ src/constants/RDP02_INSTANCES_DISTRIBUTION.ts
@@ -1,25 +1,14 @@
-import { Entorno } from "../interfaces/shared/Entornos";
 import { RDP02 } from "../interfaces/shared/RDP02Instancias";
-import { ENTORNO } from "./ENTORNO";
 
+export const DIRECTIVO_INSTANCES = [RDP02.INS1];
+export const AUXILIAR_INSTANCES = [RDP02.INS1];
+export const PERSONAL_ADMIN_INSTANCES = [RDP02.INS1];
 
-export const DIRECTIVO_INSTANCES =
-  ENTORNO === Entorno.PRODUCCION ? [RDP02.INS1] : [RDP02.INS1];
-export const AUXILIAR_INSTANCES =
-  ENTORNO === Entorno.PRODUCCION ? [RDP02.INS1] : [RDP02.INS1];
-export const PERSONAL_ADMIN_INSTANCES =
-  ENTORNO === Entorno.PRODUCCION ? [RDP02.INS1] : [RDP02.INS1];
+export const PROFESOR_SECUNDARIA_INSTANCES = [RDP02.INS2];
+export const TUTOR_INSTANCES = [RDP02.INS2];
 
-export const PROFESOR_SECUNDARIA_INSTANCES =
-  ENTORNO === Entorno.PRODUCCION ? [RDP02.INS2] : [RDP02.INS1];
-export const TUTOR_INSTANCES =
-  ENTORNO === Entorno.PRODUCCION ? [RDP02.INS2] : [RDP02.INS1];
+export const PROFESOR_PRIMARIA_INSTANCES = [RDP02.INS3];
 
-export const PROFESOR_PRIMARIA_INSTANCES =
-  ENTORNO === Entorno.PRODUCCION ? [RDP02.INS3] : [RDP02.INS1];
-
-
-  
 // Mapa que relaciona cada instancia con su URL de conexión
 export const RDP02_INSTANCES_DATABASE_URL_MAP: Map<RDP02, string | undefined> =
   new Map([
