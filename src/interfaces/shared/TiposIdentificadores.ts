656a65596f4222e5c81aab1006d09e35a17b8e1e
GeoCoderDev <juanchavezsaldana1@gmail.com>
2025-08-05 23:42:37 -0500

    Nueva version de TPS01 migrado para Ids en vez de DNIs

--- src/interfaces/shared/TiposIdentificadores.ts
+++ src/interfaces/shared/TiposIdentificadores.ts
@@ -1,12 +1,11 @@
 export enum TiposIdentificadores {
-    DNI=1,
-    CE=2,
-
+  DNI = 1,
+  CARNET_EXTRANJERIA = 2,
+  CODIGO_ESCUELA = 3
 }
-
-
 
 export const TiposIdentificadoresTextos: Record<TiposIdentificadores, string> = {
-    [TiposIdentificadores.DNI]: "DNI",
-    [TiposIdentificadores.CE]: "CE",
-}
+  [TiposIdentificadores.DNI]: "DNI",
+  [TiposIdentificadores.CARNET_EXTRANJERIA]: "Carnet de Extranjería",
+  [TiposIdentificadores.CODIGO_ESCUELA]: "Código de Escuela",
+};
